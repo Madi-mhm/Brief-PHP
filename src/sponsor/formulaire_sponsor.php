11d634775ee063d1b33e58abdbf33c9bea077b2a
Mahdi <mahdi.unic1@gmail.com>
2023-04-25 14:19:27 +0200

    nothing here

--- src/sponsor/formulaire_sponsor.php
+++ src/sponsor/formulaire_sponsor.php
@@ -6,8 +6,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./sponsor.css">
-    <title>Brief php</title>
+    <link rel="stylesheet" href="./formulaire_sponsor.css">
+    <title>Formulaire Sponsor</title>
 </head>
 <body>
     <section class="page">
