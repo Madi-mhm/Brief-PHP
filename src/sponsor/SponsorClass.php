23e06b68cbc469a64bed8dfab4ba4e5b01d9578a
Mahdi <mahdi.unic1@gmail.com>
2023-04-25 16:09:04 +0200

    done

---
+++ src/sponsor/SponsorClass.php
@@ -0,0 +1,20 @@
+<?php 
+class Sponsor {
+    private $id; 
+    private $brand; 
+
+    public function getId(){
+        return $this->id;
+    };
+    public function setId($id){
+        $this -> id = $id;
+    };
+    public function getBrand(){
+        return $this->id;
+    };
+    public function setBrand($brand){
+        $this -> brand = $brand;
+    };
+};
+
+?>
