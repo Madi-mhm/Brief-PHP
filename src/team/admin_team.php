ab287084f85a6b4791faf7cb75675de5ce1776b3
JDeniau <jerome.deniau.simplon@gmail.com>
2023-04-25 15:17:20 +0200

    php

---
+++ src/team/admin_team.php
@@ -0,0 +1,38 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="admin_team.css">
+    <script type="module" src="game.js"></script> 
+    <title>Brief php</title>
+</head>
+<body>
+    <section class="page_admin">
+        <h1>Administration team page</h1>
+        <div class="tab">
+          <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Description</th>
+            </tr>
+        </thead>
+        <tbody>
+            <!-- Les données de la base de données devraient être insérées ici -->
+            <tr>
+                <td>1</td>
+                <td>League of Legends</td>
+                <td>Jeux PC</td>
+
+            </tr>
+        </tbody>
+    </table>
+        </div>
+    </section>
+</body>
+</html>
