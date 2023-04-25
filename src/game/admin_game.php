c065799f780ae99f86ddc1b14ca81a690d7632c0
tom <tom.pustel.simplon@gmail.com>
2023-04-25 14:22:22 +0200

    page admin_game en html/css

---
+++ src/game/admin_game.php
@@ -0,0 +1,39 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="game.css">
+    <script type="module" src="game.js"></script> 
+    <title>Brief php</title>
+</head>
+<body>
+    <section class="page_admin">
+        <h1>Administration page game</h1>
+        <div class="tab">
+          <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nom</th>
+                <th>Station</th>
+                <th>Format</th>
+            </tr>
+        </thead>
+        <tbody>
+            <!-- Les données de la base de données devraient être insérées ici -->
+            <tr>
+                <td>1</td>
+                <td>League of Legends</td>
+                <td>PC</td>
+                <td>MOBA</td>
+            </tr>
+        </tbody>
+    </table>
+        </div>
+    </section>
+</body>
+</html>
