638dc5f2b3517191f464b592a223443fbeda17d4
Mahdi <mahdi.unic1@gmail.com>
2023-04-28 16:43:28 +0200

    save the code

---
+++ src/sponsor/sponsor.js
@@ -0,0 +1,16 @@
+const editButton = document.getElementById('editButton').addEventListener("click", ()=>{
+
+    // sponsorPopup.classList.remove('hide');
+    console.log("test");
+    
+    
+});
+
+
+
+
+
+const sponsorPopup = document.querySelector('.sponsorPopup')
+
+
+
