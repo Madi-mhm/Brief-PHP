02f3dc0b2eec0b9cfb0e562be4b6bc35e291cebc
tom <tom.pustel.simplon@gmail.com>
2023-04-28 16:52:11 +0200

    fonction édition de la page player

--- src/player/admin_player.php
+++ src/player/admin_player.php
@@ -9,6 +9,37 @@
     $managerPlayer->delete($_GET['delete']);
  }
 
+$allPlayers = $managerPlayer->getAllPlayers();
+$playerToEdit = null;
+if (isset($_GET['edit'])) {
+    $playerId = $_GET['edit'];
+    $playerToEdit = $managerPlayer->findById($playerId);
+}
+
+$allTeams = $managerPlayer->getAllTeams();
+$teamToEdit = null;
+if (isset($_GET['edit'])) {
+    $teamId = $_GET['edit'];
+    $teamToEdit = $managerPlayer->findById($teamId);
+}
+
+$allGames = $managerPlayer->getAllGames();
+$gameToEdit = null;
+if (isset($_GET['edit'])) {
+    $gameId = $_GET['edit'];
+    $gameToEdit = $managerPlayer->findById($gameId);
+}
+
+if (isset($_POST['update'])) {
+    $playerId = $_POST['id'];
+    $first_name = $_POST['first_name'];
+    $second_name = $_POST['second_name'];
+    $city = $_POST['city'];
+    $team_id = $_POST['team_id'];
+    $game_id = $_POST['game_id'];
+    $managerPlayer->edit($playerId, $first_name, $second_name, $city, $team_id, $game_id);
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -45,16 +76,12 @@
                   <td><?php echo $player->getCity(); ?></td>
                   <td><?php echo $player->getTeamId(); ?></td>
                   <td><?php echo $player->getGameId(); ?></td>
-                  <td><a href="admin_player.php?delete=<?php echo $player->getId(); ?>" class="trash"></a></td>
-                </tr>
-              <?php } ?>
-              <?php if (isset($_POST['first_name'])) { ?>
-                <tr>
-                  <td><?php echo $_POST['first_name']; ?></td>
-                  <td><?php echo $_POST['second_name']; ?></td>
-                  <td><?php echo $_POST['city']; ?></td>
-                  <td><?php echo $_POST['team_id']; ?></td>
-                  <td><?php echo $_POST['game_id']; ?></td>
+                  <td>
+                  <section class="crudButton">    
+        <a href="admin_player.php?delete=<?php echo $player->getId(); ?>" class="trash"></a>
+        <a href="admin_player.php?edit=<?php echo $player->getId(); ?>" class="edit"></a>
+            </section>
+        </td>
                 </tr>
               <?php } ?>
             </tbody>
@@ -62,5 +89,54 @@
           <a href="./formulaire_player.php"><button class="btn">RETOUR</button></a>
         </div>
     </section>
+
+    <!-- POPPUP -->
+    <section id="editPoppup" class="editPoppup">
+        <div class="poppupContainer">
+        <form method="POST" action="admin_player.php">
+           <h1>Equipes</h1>
+           <div class="séparation">
+            <div class="corps-formulaire">
+                <div class="contenu">
+                    <div class="boite">
+                        <label for="first_name">Prénom</label>
+                        <input type="text" name="first_name" value="<?php echo $playerToEdit ? $playerToEdit->getFirstName() : ''; ?>">
+                    </div>
+                    <div class="boite">
+                        <label for="second_name">Nom</label>
+                        <input type="text" name="second_name" value="<?php echo $playerToEdit ? $playerToEdit->getSecondName() : ''; ?>">
+                    </div>
+                    <div class="boite">
+                        <label for="city">Ville</label>
+                        <input type="text" name="city" value="<?php echo $playerToEdit ? $playerToEdit->getCity() : ''; ?>">
+                    </div>
+                    <div class="boite dropDown">
+                        <label for="team_id">Equipe</label>
+                        <select type="select" name="team_id">
+                           <?php foreach($allTeams as $team) { ?>
+                                  <option value="<?php echo $teamToEdit ? $teamToEdit->getTeamId() : ''; ?>"><?= $team->getName() ?></option>
+                           <?php } ?>
+                        </select>
+                    </div>
+                    <div class="boite">
+                        <label for="game_id">Jeu</label>
+                        <select type="select" name="game_id">
+                            <?php foreach($allGames as $game) { ?>
+                                   <option value="<?php echo $gameToEdit ? $gameToEdit->getGameId() : ''; ?>"><?= $game->getName() ?></option>
+                            <?php } ?>
+                        </select>
+                    </div>
+                    <input type="hidden" name="id" value="<?php echo $playerToEdit ? $playerToEdit->getId() : ''; ?>">
+
+                </div>
+            </div>
+            <div class="pied-formulaire">
+            <a href="admin_player.php"><button class="cancelButton" type="button"><strong>Cancel</strong></button></a>
+                <button class="updateButton" name="update"><strong>Update</strong></button>
+            </div>
+           </div>
+        </form>
+            <div>
+        </section>
 </body>
 </html>
